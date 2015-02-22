b881c2927872d170dad5f958c89658f53ea6562c
Davis Freimanis <davisfreimanis@gmail.com>
2015-02-22 21:38:24 +0100

    added search path for html files

--- zibenitis/settings.py
+++ zibenitis/settings.py
@@ -77,8 +77,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Template files like .html
+# https://docs.djangoproject.com/en/1.7/intro/tutorial02/
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
