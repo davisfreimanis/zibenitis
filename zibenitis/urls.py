857193f6f23892b24237e2fc93524d02d126c80c
Kaspars Skels <kaspars.skels@gmail.com>
2015-02-23 21:26:11 +0100

    Renamed dejotaju_saraksts => dancers

--- zibenitis/urls.py
+++ zibenitis/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'zibenitis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^dancers/', include('dejotaju_saraksts.urls')),
+    url(r'^dancers/', include('dancers.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
