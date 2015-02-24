cbf385871a956f755f911306fc4f9cfac7244e25
Kaspars Skels <kaspars.skels@gmail.com>
2015-02-24 22:37:33 +0100

    First views; renamed to news => blog

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+from blog import views
+
+urlpatterns = patterns('',
+    # ex: /blog/
+    url(r'^$', views.index, name='index'),
+    # ex: /blog/5/
+    url(r'^(?P<blog_id>\d+)/$', views.detail, name='detail')
+)
+
--- zibenitis/urls.py
+++ zibenitis/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^dancers/', include('dancers.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
