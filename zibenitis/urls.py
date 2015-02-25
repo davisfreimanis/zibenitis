86f0399f83f43b7d2c55b12058b460042ebf9d4f
Kaspars Skels <kaspars.skels@gmail.com>
2015-02-25 23:01:56 +0100

    Basic blog functionality

--- zibenitis/urls.py
+++ zibenitis/urls.py
@@ -3,10 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'zibenitis.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^dancers/', include('dancers.urls')),
+    url(r'^$', include('blog.urls'), name='home'), # think about this!
     url(r'^blog/', include('blog.urls')),
+    url(r'^dancers/', include('dancers.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
