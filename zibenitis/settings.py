5007580a82d02d0901a8d11f77b23ce7a500bcec
Kaspars Skels <kaspars.skels@gmail.com>
2015-02-28 00:09:57 +0100

    Set static file root folder, needed for Apache2

--- zibenitis/settings.py
+++ zibenitis/settings.py
@@ -88,3 +88,4 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
