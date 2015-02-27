ec9522e341067554480c8b4f62ea5efe7c73621a
Ugis Lacis <ugis.lacis@gmail.com>
2015-02-27 00:43:42 -0500

    Created basic web sesponse for dancers app, added blog post self-naming function for easier administration in admin panel. Removed middle name from person database.

--- dancers/views.py
+++ dancers/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Very basic, test view
+from dancers.models import Person
+
+## Very basic, test view
+#def index(request):
+#    return HttpResponse("Hello, you are at list of dance group participants")
+
 def index(request):
-    return HttpResponse("Hello, you are at list of dance group participants")
+    persons = Person.objects.order_by('last_name')
+    return render(request, 'dancers/index.html', {'persons': persons})
