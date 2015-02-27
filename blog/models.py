2900b653dc2f45678c2d9888dfd23f1c7e01eb31
Davis Freimanis <davisfreimanis@gmail.com>
2015-02-27 22:14:12 +0100

    small changes to blog files

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-
 
     def get_absolute_url(self):
         return reverse('blog.views.post', args=[self.title])
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
+
 def index(request):
     posts = Post.objects.order_by('-created')
     return render(request, 'blog/blog.html', {'posts': posts})
+
 
 def post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
