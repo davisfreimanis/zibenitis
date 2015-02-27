f3f33fff823967f8ea83648d7ae63a03723f76a7
Ugis Lacis <ugis.lacis@gmail.com>
2015-02-27 01:20:23 -0500

    Test edit for branch bootstrap.

--- dancers/views.py
+++ dancers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Very basic, test view
+# Very basic, test view, nothing to return really
 def index(request):
     return HttpResponse("Hello, you are at list of dance group participants")
