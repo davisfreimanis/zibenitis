a36b78b916c4e4ea53792368fa8ec884509242d4
Davis Freimanis <davisfreimanis@gmail.com>
2015-02-28 20:24:29 +0100

    added an event app for upcoming events

--- dancers/models.py
+++ dancers/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # We define a single class, which corresponds to enteries of each person.
 
+
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name  = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     date_added = models.DateField()
+
     def __str__(self):
         return self.first_name+" "+self.last_name
 
---
+++ events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=2500)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.event_title
+
