11744277ec4c5de44babfc446c4a5f38b3262166
Ugis Lacis <ugis.lacis@gmail.com>
2015-03-05 11:23:06 +0100

    Fixed dancers view, removed form, which wastn't working, and implemented basic picture display. The database is modified by moving picture field to a person model, and adding a default (dummy) picture for persons, which initially does not have a picture.

--- dancers/forms.py
+++
@@ -1,8 +0,0 @@
-# Form to accept file upload
-
-from django import forms
-
-class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    pic = forms.FileField(label='Select a file')
-
--- dancers/views.py
+++ dancers/views.py
@@ -2,32 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from dancers.models import Person
-from dancers.models import Picture
-from dancers.forms import UploadFileForm
 
 
 def index(request):
     persons = Person.objects.order_by('last_name')
-#    return render(request, 'dancers/index.html', {'persons': persons})
+    return render(request, 'dancers/index.html', {'persons': persons})
 
-    # Handle file upload
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        datatxt = "Here comes debugging output, I am at 1, form.is_valid = %d" %( form.is_valid() )
-        if form.is_valid():
-            newpic = Picture(pic = request.FILES['pic'])
-            newpic.save()
-            datatxt = " %s." %(newpic.filename) 
-
-           # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('dancers.views.index'))
-
-    else:
-        form = UploadFileForm() # A empty, unbound form
-        datatxt = "Here comes debugging output, I am at 2"
-
-    return render(
-        request,
-        'dancers/index.html',
-        {'persons': persons, 'form': form, 'data': datatxt}
-    )
