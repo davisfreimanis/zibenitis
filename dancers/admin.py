4dfa0574aebd2a962b124204139292cea1bee721
Ugis Lacis <ugis.lacis@gmail.com>
2015-03-05 11:23:06 +0100

    Fixed dancers view, removed form, which wastn't working, and implemented basic picture display. The database is modified by moving picture field to a person model, and adding a default (dummy) picture for persons, which initially does not have a picture.

--- dancers/admin.py
+++ dancers/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from dancers.models import Person
-from dancers.models import Picture
 
 # Register your models here.
 admin.site.register(Person)
-admin.site.register(Picture)
--- dancers/models.py
+++ dancers/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+from django.conf import settings
+from datetime import datetime
+
 # We define a single class, which corresponds to enteries of each person.
 
 
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
+    mid_name = models.CharField(max_length=50,blank=True)
     last_name = models.CharField(max_length=50)
-    date_added = models.DateField()
+    date_added = models.DateField(default=datetime.now())
+    picture = models.ImageField(upload_to='dancers_pic', max_length=500, default='dancers_pic/default_pic.png' )
 
     def __str__(self):
         return self.first_name+" "+self.last_name
-
-class Picture(models.Model):
-    pic = models.ImageField(upload_to='dancers_pic', max_length=500)
 
