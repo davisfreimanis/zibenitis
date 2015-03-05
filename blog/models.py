22511fbead8ee6660c459d964c982523d84e6611
Kaspars Skels <kaspars.skels@gmail.com>
2015-03-05 22:20:16 +0100

    Make datetime settable for blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django_markdown.models import MarkdownField
 from django.db import models
 from django.core.urlresolvers import reverse
+from datetime import datetime
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = MarkdownField()
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=datetime.now())
 
     def __str__(self):
         return self.title
