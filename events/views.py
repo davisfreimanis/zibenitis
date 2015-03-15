5cc5b1d957f8fb45083e7bdea89b310cd3eea457
Davis Freimanis <davisfreimanis@gmail.com>
2015-03-15 19:13:10 +0100

    Fixed link to separate event page

--- events/views.py
+++ events/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from events.models import Event
 
 
+# For the sidebar (brief information)
 def event_brief(request):
     events = Event.objects.order_by('date')
     # events = Event.objects.all()  # order_by('events__event_date') //not working?
     return render(request, 'events/events.html', {'events': events})  # A list for the side bar. Title and date
 
 
+# A detailed page with only the event
 def event_detail(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     return render(request, 'events/event.html', {'event': event})  # An event with description, time, location and more
