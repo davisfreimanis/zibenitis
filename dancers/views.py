09f8d9fd24089e54ccdcf37dbb69f730f4a5a06f
Ugis Lacis <ugis.lacis@gmail.com>
2015-05-04 19:20:35 +0200

    United about us and and contact us structures, added respective database entries for contact persons.

--- dancers/views.py
+++ dancers/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from dancers.models import History
 from dancers.models import Person
-
+from dancers.models import Contact_person
 
 def index(request):
-    persons = Person.objects.order_by('last_name')
-    return render(request, 'dancers/index.html', {'persons': persons})
+    history   = History.objects.order_by('modified')
+    persons   = Person.objects.order_by('last_name')
+    cont_pers = Contact_person.objects.order_by('role')
+    return render(request, 'dancers/index.html', {'persons': persons, 'history': history, 'cpers': cont_pers})
 
