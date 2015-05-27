bb447ce7054594f47a091f479e93cee1f31d500d
Davis Freimanis <davisfreimanis@gmail.com>
2015-05-27 20:58:59 +0200

    added google maps map to event view

--- dancers/views.py
+++ dancers/views.py
@@ -5,9 +5,10 @@ from dancers.models import History
 from dancers.models import Person
 from dancers.models import Contact_person
 
+
 def index(request):
-    history   = History.objects.order_by('modified')
-    persons   = Person.objects.order_by('last_name')
+    history = History.objects.order_by('modified')
+    persons = Person.objects.order_by('last_name')
     cont_pers = Contact_person.objects.order_by('role')
     return render(request, 'dancers/index.html', {'persons': persons, 'history': history, 'cpers': cont_pers})
 
