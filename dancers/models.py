da85d1b54e0b3980dc2c0026f92d463d74f61b92
Kaspars Skels <kaspars.skels@gmail.com>
2015-06-10 22:50:39 +0200

    Add default value for contact order

--- dancers/models.py
+++ dancers/models.py
@@ -38,7 +38,7 @@ class Person(models.Model):
 class Contact_person(models.Model):
     person = models.ForeignKey(Person)
     role = models.CharField(max_length=100)
-    order = models.IntegerField()
+    order = models.IntegerField(default=1)
     
     def __str__(self):
         return self.role+" "+self.person.first_name
