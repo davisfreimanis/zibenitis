846c4ad81419c465d5f1ebde70c7881d09a32e57
Davis Freimanis <davisf@kth.se>
2017-03-11 11:50:39 +0100

    Removed gallery

--- events/models.py
+++ events/models.py
@@ -3,12 +3,12 @@ from datetime import datetime, timedelta, time
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=2500)
-    date = models.DateTimeField()
-    duration = models.FloatField(default=1)
-    location = models.CharField(max_length=50)
-    information = models.CharField(max_length=50, default="")
+    title = models.CharField(max_length=50, help_text="Pasākuma nosaukums")
+    description = models.TextField(max_length=2500, help_text="Pasākuma apraksts")
+    date = models.DateTimeField(help_text="Datums un pūlkstenis kad pasākums sāksies")
+    duration = models.FloatField(default=5, help_text="Pasākuma ilgums stundās")
+    location = models.CharField(max_length=50, help_text="Adrese ko Google maps mēģinās atrast")
+    information = models.CharField(max_length=50, default="", blank=True, help_text="Links kur var atrast vairāk informāciju")
 
     def __str__(self):
         return self.title
