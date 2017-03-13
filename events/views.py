9b45857e4d527aed08e6bedbb625aaf790baa1fb
Davis Freimanis <davisf@kth.se>
2017-03-13 21:51:10 +0100

    SCSS fix

--- events/views.py
+++ events/views.py
@@ -11,9 +11,21 @@ def event_brief(request):
     upcoming_events = Event.objects.filter(date__gte=now).order_by('date')  # posts that are upcoming
     passed_events = Event.objects.filter(date__lte=now).order_by('-date')
 
-    return render(request, 'events/events.html', {'events': upcoming_events, 'passed_events': passed_events, 'event_active': active})  # A list for the side bar. Title and date
+    context = {
+        'events': upcoming_events,
+        'passed_events': passed_events,
+        'event_active': active
+    }
+
+    return render(request, 'events/events.html', context)  # A list for the side bar. Title and date
 
 
 def event_detail(request, event_id):
     event = get_object_or_404(Event, id=event_id)
-    return render(request, 'events/event.html', {'event': event, 'event_active': active})  # An event with description, time, location and more
+
+    context = {
+        'event': event,
+        'event_active': active
+    }
+
+    return render(request, 'events/event.html', context)  # An event with description, time, location and more
