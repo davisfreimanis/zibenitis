de55dee2b0a28169c3d5e7d894acb516686b8da4
Davis Freimanis <davisf@kth.se>
2017-03-14 10:49:46 +0100

    admin link fix

--- zibenitis/settings.py
+++ zibenitis/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django_markdown',
     'dancers',
     'blog',
