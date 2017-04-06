316b154bd46b6a670bf0c2c4fb6031895fdb63fb
Davis Freimanis <davisf@kth.se>
2017-04-06 20:33:36 +0200

    stats counts rehearsal visits

--- stats/urls.py
+++ stats/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from stats import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='stats'),
-    url(r'^new/', views.RehearsalCreate.as_view(), name='rehearsal-add'),
-    # url(r'^update/',),
-    # url(r'^delete/',),
+    url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.RehearsalCreate.as_view(), name='rehearsal-add'),
+    url(r'^(?P<pk>[0-9]+)$', views.RehearsalUpdate.as_view(), name='rehearsal-update'),
+    url(r'^(?P<pk>[0-9]+)/delete$', views.RehearsalDelete.as_view(), name='rehearsal-delete'),
 ]
