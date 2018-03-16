3abd3dd58d586a567e13059200eecc96fccd5eda
Davis Freimanis <davisfreimanis@gmail.com>
2018-03-16 18:26:24 +0100

    Old file removal

--- zibenitis/apps/stats/urls.py
+++ zibenitis/apps/stats/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', views.RehearsalCreate.as_view(), name='rehearsal-add'),
     path('<int:pk>/', views.RehearsalUpdate.as_view(), name='rehearsal-update'),
     path('<int:pk>/delete', views.RehearsalDelete.as_view(), name='rehearsal-delete'),
+
 ]
