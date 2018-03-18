8d3d3d2749cc5ca8ab83b814ce8a239793f7a81d
Davis Freimanis <davisfreimanis@gmail.com>
2018-03-18 23:11:44 +0100

    Removed old photologue templates and new background

---
+++ static-loc/js/photoswipe.js
@@ -0,0 +1,13 @@
+var openPhotoSwipe = function(startIndex) {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // define options (if needed)
+    var options = {
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0,
+        index: startIndex
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, window.djangoImages, options);
+    gallery.init();
+};
